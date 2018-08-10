36678b57d65883db9395bf2f01908b854184c790
leomenegon <leo.menegon3@gmail.com>
2018-08-09 22:14:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>GameStore - Games, Notícias, Reviews e Mais!</title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>    
+<!-- 	<header id="faixa_top">
+		<div class="container">
+            <div class="col-12">
+                <div id="logo"><img src="images/logo.png" alt="Logo" rel="GameStore" height="56px" /></div>
+		<nav id="menu" class="navbar navbar-dark">				
+				    <ul class="btmenu">					
+					   <li class="nav-item"><a href="#">HOME</a></li>
+					   <li><a href="#">GAMES</a></li>
+					   <li><a href="#">GALERIA</a></li>
+					   <li><a href="#">SOBRE</a></li>
+					   <li><a href="#">CONTATO</a></li>					
+				    </ul>		
+ 	</nav>
+	</div>
+
+		  </div>
+		</header> -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container">
+
+  <a class="navbar-brand p-0 mb-0 mt-0" href="#"><img src="images/logo.png" alt="logo" height='48px' class="m-0 p-0"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Games</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Galeria</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sobre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="noticia.php">Teste</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+						</div>
+
+</nav>
+	
+	
+		<div class="container">
+	<!-- Fazer caminho do diretório aqui (notícias>exemplo) -->
+			<section class="col-lg-7 float-left mt-3 col-sm-12 p-0">
+				<figure class="img-thumbnail clicavel p-0">
+                    <img src="images/news/gow0.png" class="img-fluid p-0">
+					<figcaption class="caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit.ipsum dolor sit amet, consectetur adipiscing elit.ipsum dolor sit amet, consectetur adipiscing elit.ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+				</figure>
+			</section>
+			<section class="col-lg-5 float-right mt-3 col-sm-12 pr-0 pl-1">
+				<figure class="col-md-6 img-thumbnail float-right gapmed p-0 clicavel"> <div class="img-fluid"><img src="images/news/detroit.jpg" class="img-fluid"> <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>  </div> </figure>
+				<figure class="col-md-6 img-thumbnail float-right gapmed p-0 clicavel"> <div class="img-fluid"><img src="images/news/fort.jpg" class="img-fluid"> <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>  </div> </figure>
+				<figure class="col-md-6 img-thumbnail float-right m-0 p-0 clicavel"> <div class="img-fluid"><img src="images/news/lol.jpg" class="img-fluid"> <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>  </div> </figure>
+				<figure class="col-md-6 img-thumbnail float-right m-0 p-0 clicavel"> <div class="img-fluid"><img src="images/news/blanka.jpg" class="img-fluid"> <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>  </div> </figure>
+				
+				<div class="col-md-12 float-left text-center vermais p-1">
+					<a href="#">VER MAIS</a>
+				</div>
+			</section>
+				<div class="col-md-12 float-left text-center vermais2 p-1">
+					<a href="#">VER MAIS</a>
+				</div>
+
+		</div>  
+	<section class="container">
+	
+		<fieldset class="col-xl-7 col-lg-12 float-left mt-3 mb-3 col-sm-12">
+			<legend id="recentes" style="margin-bottom: 0">MAIS RECENTES</legend> <div class="linha"></div>
+            
+            
+            <article class="recente float-left clicavel" style="border-top: 0px">
+                <figure class="imgrec float-left m-2">
+                    <img src="images/placeholder.png" alt="Placeholder" class="media float-left img-fluid">
+                </figure>
+                <div class="float-right txtrec pr-2">
+                    <h5 class="text-center mt-2">Título</h5>
+                    <p class="media-body">
+                        &emsp; Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+                    </p>
+                </div>
+            </article>
+            <article class="recente float-left clicavel" style="border-top: 0px">
+                <figure class="imgrec float-left m-2">
+                    <img src="images/placeholder.png" alt="Placeholder" class="media float-left img-fluid">
+                </figure>
+                <div class="float-right txtrec pr-2">
+                    <h5 class="text-center mt-2">Título</h5>
+                    <p class="media-body">
+                        &emsp; Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+                    </p>
+                </div>
+            </article>
+            <article class="recente float-left clicavel" style="border-top: 0px">
+                <figure class="imgrec float-left m-2">
+                    <img src="images/placeholder.png" alt="Placeholder" class="media float-left img-fluid">
+                </figure>
+                <div class="float-right txtrec pr-2">
+                    <h5 class="text-center mt-2">Título</h5>
+                    <p class="media-body">
+                        &emsp; Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+                    </p>
+                </div>
+            </article>
+            <article class="recente float-left clicavel" style="border-top: 0px">
+                <figure class="imgrec float-left m-2">
+                    <img src="images/placeholder.png" alt="Placeholder" class="media float-left img-fluid">
+                </figure>
+                <div class="float-right txtrec pr-2">
+                    <h5 class="text-center mt-2">Título</h5>
+                    <p class="media-body">
+                        &emsp; Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+                    </p>
+                </div>
+            </article>
+            <article class="recente float-left clicavel" style="border-top: 0px">
+                <figure class="imgrec float-left m-2">
+                    <img src="images/placeholder.png" alt="Placeholder" class="media float-left img-fluid">
+                </figure>
+                <div class="float-right txtrec pr-2">
+                    <h5 class="text-center mt-2">Título</h5>
+                    <p class="media-body">
+                        &emsp; Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+                    </p>
+                </div>
+            </article>
+            <article class="recente float-left clicavel" style="border-top: 0px">
+                <figure class="imgrec float-left m-2">
+                    <img src="images/placeholder.png" alt="Placeholder" class="media float-left img-fluid">
+                </figure>
+                <div class="float-right txtrec pr-2">
+                    <h5 class="text-center mt-2">Título</h5>
+                    <p class="media-body">
+                        &emsp; Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.
+                    </p>
+                </div>
+            </article>
+
+		</fieldset> 
+
+        <div id="page_num"></div>
+		
+		<div class="d-none d-xl-block">
+	<aside class="col-4 float-right mt-3 mr-0 ml-0 p-0 text-center">
+        <img src="images/placeholder2.png" alt="placeholder" class="media img-fluid m-auto">
+        </aside>
+	<aside id="content_pool" class="col-4 float-right mt-3 ml-3 votacao">
+        <h4 class="text-center">Melhor do Mês!</h4>
+        <ul style="list-style: none">
+            <li><input type="radio" name="voto1">Teste</li>
+            <li><input type="radio" name="voto2">Teste</li>
+            <li><input type="radio" name="voto3">Teste</li>
+            <li><input type="radio" name="voto4">Teste</li>
+			<li><input type="submit"></li>
+        </ul>
+	</aside>
+	<aside class="col-4 float-right mt-3 ml-3 gameright">
+        <ul style="list-style: none;padding-left: 0px;">
+            <div class="float-left linha1 clicavel mt-1"><li><img src="images/placeholder2.png" alt="placeholder" width="120px" class="media m-1 mr-2 float-left"><div class="p-2"><h5>Título</h5>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi.</div></li></div>
+            <div class="float-left linha1 clicavel"><li><img src="images/placeholder2.png" alt="placeholder" width="120px" class="media m-1 mr-2 float-left"><div class="p-2"><h5>Título</h5>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. </div></li></div>
+            <div class="float-left linha1 clicavel mb-1 border-bottom-0"><li style="style=border-bottom: 0px"><img src="images/placeholder2.png" alt="placeholder" width="120px" class="media m-1 float-left mr-2"><div class="p-2"><h5>Título</h5>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi.</div></li></div>
+        </ul>
+	</aside>
+        </div>
+    </section>
+	<footer id="faixa_bottom" class="mt-4">
+		<div class="container"><div class=""></div>
+        <ul class="col-12 navbar ulbt mt-3 p-0 text-center">
+            <li><a href="https://facebook.com" target="_blank"><img src="images/face1.png" width="16px" class="float-left media">&nbsp; facebook.com/GameStore</a></li>
+            <li><a href="https://twitter.com" target="_blank"><img src="images/twitter1.png" width="16px" class="float-left media">&nbsp;twitter.com/GameStore</a></li>
+            <li class="text-center"><a href="" target="_blank">Políticas de Privacidade</a></li>
+            <li><a href="" target="_blank">Termos de Uso</a></li>
+        </ul></div>
+	</footer>
+	<footer class="col-12 float-left text-center copybt">
+        &copy; 2018 Leonardo Menegon. Todos os direitos reservados.
+	</footer>
+	
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ noticia.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>GameStore - Games, Notícias, Reviews e Mais!</title>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>    
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div class="container">
+
+  <a class="navbar-brand p-0 mb-0 mt-0" href="#"><img src="images/logo.png" alt="logo" height='48px' class="m-0 p-0"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Games</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Galeria</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Sobre</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contato</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+						</div>
+
+</nav>
+ 
+	<section class="container">
+	
+		<section class="col-xl-9 col-lg-12 float-left mt-3 mb-3 col-sm-12">
+			<main>
+				<header id="titulonot" class="text-center"><h1>Teste</h1></header>
+				<article id="corponot" class="p-2 text-justify">
+					<figure id="imgnot">
+						<img src="images/18839.jpg" alt="imagem" class="img-fluid">
+					</figure>
+<p>Silvio Santos Ipsum mah voc mora com o papai ou com a mamem? Estamos em ritmo de festamm. O prmio  em barras de ouro, que vale mais que dinheiroam. Mah voc no consegue n Moiss? Voc no consegueam. Ma vai pra l. Voc veio da caravana de ondeammm? Mah ooooee vem pra c. Vem pra c. Estamos em ritmo de festamm. Ma voc, topa ou no topamm.</p>
+
+  <p>Por sua conta e riscoamm? Mah voc mora com o papai ou com a mamem? Estamos em ritmo de festamm. Qual  a musicamm? Ha hai. Bem boladoam, bem boladoam. Bem gozadoam. Ma vale drreaisam?  fcil ou no am? Wellintaaammmmmmmmm. Ma voc est certo dissoam? Mah voc mora com o papai ou com a mamem? Ma quem quer dinheiroam?</p>
+
+ <p>Qual  a musicamm?  bom ou no am? Boca sujuam... sem vergonhuamm. Ma tem ou no tem o celular do milhouamm? Voc veio da caravana de ondeammm? Eu no queria perguntar isso publicamente, ma vou perguntar. Carla, voc tem o ensino fundamentauam? O arriscam tuduam, valendo um milho de reaisuam. Ma vai pra l.  dinheiro ou no am?</p>
+
+ <p>Vem pra l, mah voc vai pra c. Agora vai, agora vem pra lamm.  namoro ou amizadeemm? Ma no existem mulher feiam, existem mulher que no conhece os produtos Jequitiamm. Ma quem quer dinheiroam?  por sua conta e riscoamm? Ma voc est certo dissoam? Eu s acreditoammmm.... Vendoammmm. O prmio  em barras de ouro, que vale mais que dinheiroam. Voc veio da caravana de ondeammm?</p>
+				</article>
+				<footer id="rodapenot"></footer>
+			</main>
+		</section> 
+		
+		<div class="d-none d-xl-block">
+	<aside class="col-3 float-right mt-4 ml-0 mr-0 p-0">
+        <img src="images/placeholder2.png" alt="placeholder" class="media img-fluid">
+        </aside>
+	<aside id="content_pool" class="col-3 float-right mt-3 votacao">
+        <h4 class="text-center">Melhor do Mês!</h4>
+        <ul style="list-style: none">
+            <li><input type="radio" name="voto1">Teste</li>
+            <li><input type="radio" name="voto2">Teste</li>
+            <li><input type="radio" name="voto3">Teste</li>
+            <li><input type="radio" name="voto4">Teste</li>
+        </ul>
+	</aside>
+	<aside class="col-3 float-right mt-3 gameright">
+        <ul style="list-style: none;padding-left: 0px;">
+            <div class="float-left linha1 clicavel mt-1"><li><img src="images/placeholder2.png" alt="placeholder" width="120px" class="media m-1 mr-2 float-left"><div class="p-2"><h5>Título</h5>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi.</div></li></div>
+            <div class="float-left linha1 clicavel"><li><img src="images/placeholder2.png" alt="placeholder" width="120px" class="media m-1 mr-2 float-left"><div class="p-2"><h5>Título</h5>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. </div></li></div>
+            <div class="float-left linha1 clicavel mb-1 border-bottom-0"><li style="border-bottom: 0px"><img src="images/placeholder2.png" alt="placeholder" width="120px" class="media m-1 float-left mr-2"><div class="p-2"><h5>Título</h5>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi.</div></li></div>
+        </ul>
+	</aside>
+        </div>
+    </section>
+	<footer id="faixa_bottom" class="mt-4">
+		<div class="container text-center">
+        <ul class="col-12 navbar ulbt mt-3 p-0 text-center" style="list-style: none">
+            <li><a href="https://facebook.com" target="_blank"><img src="images/face1.png" width="16px" class="float-left media">&nbsp; facebook.com/GameStore</a></li>
+            <li><a href="https://twitter.com" target="_blank"><img src="images/twitter1.png" width="16px" class="float-left media">&nbsp;twitter.com/GameStore</a></li>
+            <li class="text-center"><a href="" target="_blank">Políticas de Privacidade</a></li>
+            <li><a href="" target="_blank">Termos de Uso</a></li>
+        </ul></div>
+	</footer>
+	<footer class="col-12 float-left text-center copybt">
+        &copy; 2018 Leonardo Menegon. Todos os direitos reservados.
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
